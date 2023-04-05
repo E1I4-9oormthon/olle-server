1666a0e092bbcca457d4df2a8b04b7ebe34c923a
YUZAMIN <untmd.crmn@gmail.com>
2023-04-06 02:32:29 +0900

    chore: Initialize Project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+
+app.get("/", function (req, res) {
+  res.send("this is home directory");
+});
+
+app.get("/write", function (req, res) {
+  res.send("this is write");
+});
