813c2b1c4cf8cbc61769d37822970a26606f5111
YUZAMIN <untmd.crmn@gmail.com>
2023-04-07 19:13:25 +0900

    feat: member model을 정의하라
    - 카카오 로그인 시 받아올 수 있는 정보를 db에 저장할 것
    Fixes: #1

---
+++ models/member.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+  const Member = sequelize.define(
+    "member",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      nickname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      profile_image: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      gender: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+      age_range: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "members",
+      deletedAt: "deletedAt",
+      paranoid: true,
+      timestamps: true,
+    }
+  );
+  return Member;
+};
