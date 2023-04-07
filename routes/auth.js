6ae7e3f8c9bd8aae138edb68c5bc9510238610d7
YUZAMIN <untmd.crmn@gmail.com>
2023-04-08 00:38:49 +0900

    feat: 카카오에서 받은 사용자의 정보를 사용한 로그인 로직을 구현하라
    - 사용자 id를 기준으로 db에 저장되어 있는지 여부를 판단
    - db에 저장되지 않은 사용자의 경우 prefer_travel을 0으로 할당 후 저장
    Fixes: #1

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const { sign } = require("../modules/jwt");
+const {
+  getAccessTokenFromKakao,
+  getUserDataFromKakao,
+} = require("./middlewares");
 
-router.get("/", (req, res, next) => {
-  res.send("Hello!");
-});
+const { Member, Olle, Apply } = require("../models");
+
+router.get(
+  "/kakao/signin/callback",
+  getAccessTokenFromKakao,
+  getUserDataFromKakao,
+  async (req, res, next) => {
+    try {
+      const accordMember = await Member.findOne({
+        where: { id: res.locals.userDataFromKakao.id },
+      });
+
+      if (!accordMember) {
+        const newMember = {
+          ...res.locals.userDataFromKakao,
+          prefer_travel: 0,
+        };
+        await Member.create(newMember);
+      }
+
+      const memberId = {
+        memberId: res.locals.userDataFromKakao.id,
+      };
+
+      const token = sign(memberId);
+
+      res.setHeader(
+        "Set-Cookie",
+        `token=${token}; Path=/; HttpOnly; SameSite=none; secure=true;`
+      );
+      return res.status(StatusCodes.OK).redirect(process.env.FRONT_URL);
+    } catch {
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .redirect(process.env.FRONT_URL);
+    }
+  }
+);
 
 module.exports = router;
