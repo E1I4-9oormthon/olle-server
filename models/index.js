b8164ce785f390bc6a7e14d665eae95674a574a4
YUZAMIN <untmd.crmn@gmail.com>
2023-04-07 20:15:08 +0900

    feat: sequelize를 사용하여 db를 정의하라
    - models/index에 각 테이블 간의 관계를 설정하여 db 정의
    Fixes: #1

--- models/index.js
+++ models/index.js
@@ -1,46 +1,52 @@
-"use strict";
-
-const fs = require("fs");
-const path = require("path");
 const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
+const path = require("path");
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
+const Olle = require("./olle");
+const Member = require("./member");
+const Apply = require("./apply");
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const config = require(path.join(__dirname, "..", "config", "database.js"))[
+  env
+];
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+const db = {
+  sequelize,
+  Sequelize,
+  Olle: Olle(sequelize, Sequelize),
+  Member: Member(sequelize, Sequelize),
+  Apply: Apply(sequelize, Sequelize),
+};
+
+db.Member.hasMany(db.Olle, {
+  foreignKey: "id",
+});
+db.Olle.belongsTo(db.Member, {
+  foreignKey: "id",
+  as: "olle_writer",
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.Member.hasMany(db.Apply, {
+  foreignKey: "id",
+});
+db.Apply.belongsTo(db.Member, {
+  foreignKey: "id",
+  as: "apply_writer",
+});
+
+db.Olle.hasMany(db.Apply, {
+  foreignKey: "id",
+  onDelete: "cascade",
+});
+db.Apply.belongsTo(db.Olle, {
+  foreignKey: "id",
+  as: "applied_olle",
+});
 
 module.exports = db;
