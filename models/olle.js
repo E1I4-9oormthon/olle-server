1b105ac893192137490ab926d76009d35a55f33a
YUZAMIN <untmd.crmn@gmail.com>
2023-04-07 19:32:21 +0900

    feat: olle model을 정의하라
    Fixes: #1

---
+++ models/olle.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const Olle = sequelize.define(
+    "olle",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      prefer_gender: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      start_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      course: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      contact: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "olles",
+      deletedAt: "deletedAt",
+      paranoid: true,
+      timestamps: true,
+    }
+  );
+  return Olle;
+};
