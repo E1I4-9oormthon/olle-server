1471fe5850ffde449b80f791db0524c1441d67f8
YUZAMIN <untmd.crmn@gmail.com>
2023-04-08 18:40:13 +0900

    fix: CORS 에러를 해결하라
    - router 연결부를 cors 객체 적용부보다 밑으로 옮겨서 해결
    Fixes: #3

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const sequelize = require("./models/index.js").sequelize;
 sequelize.sync();
 
 const authRouter = require("./routes/auth");
+const memberRouter = require("./routes/member");
 
 const app = express();
-
-app.use("/v1/api/auth", authRouter);
 
 app.use(cookieParser());
 app.use(logger("dev"));
@@ -21,17 +20,16 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      process.env.FRONT_URL,
-      "http://127.0.0.1:5173/",
-      "http://localhost:3000",
-    ],
+    origin: [process.env.FRONT_URL, "http://localhost:5173"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
+    preflightContinue: true,
     optionsSuccessStatus: 204,
     credentials: true,
   })
 );
+
+app.use("/v1/api/auth", authRouter);
+app.use("/v1/api/member", memberRouter);
 
 app.listen(8080, function () {
   console.log("listening on 8080");
