44c3370ba21db4a0d49d91f60592b6b34ef71ab5
YUZAMIN <untmd.crmn@gmail.com>
2023-04-13 01:26:47 +0900

    fix: member model의 id를 member_id로 변경하라
    Fixes: #9

--- models/member.js
+++ models/member.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Member = sequelize.define(
     "member",
     {
-      id: {
+      member_id: {
         type: DataTypes.BIGINT,
         autoIncrement: true,
         allowNull: false,
